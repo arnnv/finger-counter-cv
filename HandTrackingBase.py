4c209fc38dadd50fb829b2ca60161ece280a9337
Arnav <arnnv.g@gmail.com>
2024-07-05 15:36:40 +0530

    Complete base

--- HandTrackingBase.py
+++ HandTrackingBase.py
@@ -21,6 +21,13 @@ while True:
     pTime = cTime
 
     cv2.putText(img, f"FPS: {int(fps)}", (20, 50), cv2.FONT_HERSHEY_COMPLEX, 1, (0, 255, 0), 2)
+
+    result = hands.process(img)
+
+    if result.multi_hand_landmarks:
+        for hand_lms in result.multi_hand_landmarks:
+            mp_draw.draw_landmarks(img, hand_lms, mp_hands.HAND_CONNECTIONS)
+
     cv2.imshow("Image", img)
 
     cv2.waitKey(1)
