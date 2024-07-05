11b55a8ee83bc8d33bdd072bb260b3d824d96aaf
Arnav <arnnv.g@gmail.com>
2024-07-05 22:24:31 +0530

    Show image on cam

--- finger-counter.py
+++ finger-counter.py
@@ -1,31 +1,45 @@
+import os
 import time
 import cv2
 from modules.hand_tracking_module import HandDetector
 
 
-def main():
-    cap = cv2.VideoCapture(0)
-    pTime = 0
-
-    detector = HandDetector()
-
-    while True:
-        success, img = cap.read()
-
-        if not success:
-            break
-
-        img = detector.getHands(img)
-
-        cTime = time.time()
-        fps = 1 / (cTime - pTime)
-        pTime = cTime
-
-        cv2.putText(img, f"FPS: {int(fps)}", (20, 50), cv2.FONT_HERSHEY_COMPLEX, 1, (0, 255, 0), 2)
-        cv2.imshow("Image", img)
-
-        cv2.waitKey(1)
+def findFingerCount(img, lm_list):
+    return img
 
 
-if __name__ == "__main__":
-    main()
+IMG_FOLDER = "./img/"
+IMAGE_NAME = os.listdir(IMG_FOLDER)
+FINGER_IMAGES = []
+for img_name in IMAGE_NAME:
+    img_path = os.path.join(IMG_FOLDER, img_name)
+    finger_img = cv2.imread(img_path)
+    finger_img = cv2.resize(finger_img, (120, 120))
+    FINGER_IMAGES.append(finger_img)
+
+cap = cv2.VideoCapture(0)
+pTime = 0
+
+detector = HandDetector(detection_confidence=0.7)
+
+while True:
+    success, img = cap.read()
+    if not success:
+        break
+
+    h, w, c = FINGER_IMAGES[0].shape
+    img[0:h, 0:w] = FINGER_IMAGES[0]
+
+    img = detector.getHands(img)
+    lm_list = detector.getLandmarks(img)
+
+    img = findFingerCount(img, lm_list)
+
+    cTime = time.time()
+    fps = 1 / (cTime - pTime)
+    pTime = cTime
+
+    cv2.putText(img, f"FPS: {int(fps)}", (500, 30), cv2.FONT_HERSHEY_COMPLEX, 1, (255, 0, 0), 2)
+    cv2.imshow("Image", img)
+
+    cv2.waitKey(1)
