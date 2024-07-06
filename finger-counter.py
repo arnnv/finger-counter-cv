f8a6ebcfb3f3bf1af3065bd1058178e368debd64
Arnav <arnnv.g@gmail.com>
2024-07-06 13:54:54 +0530

    Add function for displaying finger

--- finger-counter.py
+++ finger-counter.py
@@ -11,13 +11,13 @@ def findFingerCount(lm_list):
 
     for tip in finger_tips:
         if tip == 4:
-            if lm_list[tip][0] > lm_list[tip-1][0]:
+            if lm_list[tip][0] > lm_list[tip - 1][0]:
                 open_map.append(1)
             else:
                 open_map.append(0)
             continue
 
-        if lm_list[tip][1] < lm_list[tip-2][1]:
+        if lm_list[tip][1] < lm_list[tip - 2][1]:
             open_map.append(1)
         else:
             open_map.append(0)
@@ -25,44 +25,55 @@ def findFingerCount(lm_list):
     return open_map
 
 
-IMG_FOLDER = "./img/"
-IMAGE_NAME = os.listdir(IMG_FOLDER)
-FINGER_IMAGES = []
-for img_name in IMAGE_NAME:
-    img_path = os.path.join(IMG_FOLDER, img_name)
-    finger_img = cv2.imread(img_path)
-    finger_img = cv2.resize(finger_img, (120, 120))
-    FINGER_IMAGES.append(finger_img)
+def displayFinger(img, finger_count, finger_images):
+    cv2.putText(img, f"Finger Count: {finger_count}", (120, 30), cv2.FONT_HERSHEY_PLAIN, 2, (255, 0, 0), 2)
 
-cap = cv2.VideoCapture(0)
-pTime = 0
+    h, w, c = finger_images[finger_count].shape
+    img[0:h, 0:w] = finger_images[finger_count]
 
-detector = HandDetector(detection_confidence=0.7)
+    return img
 
-while True:
-    success, img = cap.read()
-    if not success:
-        break
 
-    h, w, c = FINGER_IMAGES[0].shape
-    img[0:h, 0:w] = FINGER_IMAGES[0]
+def main():
+    IMG_FOLDER = "./img/"
+    IMAGE_NAME = os.listdir(IMG_FOLDER)
+    FINGER_IMAGES = []
+    for img_name in IMAGE_NAME:
+        img_path = os.path.join(IMG_FOLDER, img_name)
+        finger_img = cv2.imread(img_path)
+        finger_img = cv2.resize(finger_img, (120, 120))
+        FINGER_IMAGES.append(finger_img)
 
-    img = detector.getHands(img)
-    lm_list = detector.getLandmarks(img)
+    cap = cv2.VideoCapture(0)
+    pTime = 0
 
-    finger_count = 0
+    detector = HandDetector(detection_confidence=0.7)
 
-    if len(lm_list) != 0:
-        fingers_open = findFingerCount(lm_list)
-        finger_count = sum(fingers_open)
+    while True:
+        success, img = cap.read()
+        if not success:
+            break
 
-   # img = displayFinger(img, finger_count)
+        img = detector.getHands(img)
+        lm_list = detector.getLandmarks(img)
 
-    cTime = time.time()
-    fps = 1 / (cTime - pTime)
-    pTime = cTime
+        finger_count = 0
 
-    cv2.putText(img, f"FPS: {int(fps)}", (500, 30), cv2.FONT_HERSHEY_COMPLEX, 1, (255, 0, 0), 2)
-    cv2.imshow("Image", img)
+        if len(lm_list) != 0:
+            fingers_open = findFingerCount(lm_list)
+            finger_count = sum(fingers_open)
 
-    cv2.waitKey(1)
+        img = displayFinger(img, finger_count, FINGER_IMAGES)
+
+        cTime = time.time()
+        fps = 1 / (cTime - pTime)
+        pTime = cTime
+
+        cv2.putText(img, f"FPS: {int(fps)}", (500, 30), cv2.FONT_HERSHEY_COMPLEX, 1, (255, 0, 0), 2)
+        cv2.imshow("Image", img)
+
+        cv2.waitKey(1)
+
+
+if __name__ == "__main__":
+    main()
